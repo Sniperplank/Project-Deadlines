c72e321fe0fd764701b17a5d2ab22e02f02bcc22
Eilaf Aljundi <elafjundi@gmail.com>
2022-09-05 22:20:06 -0400

    logout function

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -13,8 +13,17 @@ export const signin = async (formData, navigate) => {
 export const signup = async (formData, navigate) => {
     try {
         const { data } = await axios.post('http://localhost:5000/user/signup', formData)
-        console.log('hoias')
         localStorage.setItem('profile', JSON.stringify({ ...data }))
+        navigate('/')
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const logout = (setUser, navigate) => {
+    try {
+        localStorage.clear()
+        setUser(null)
         navigate('/')
     } catch (error) {
         console.log(error)
