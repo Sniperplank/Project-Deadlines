be09716cb0b3414ea1409e4a65f0e56050983018
Eilaf Aljundi <elafjundi@gmail.com>
2022-09-07 23:20:39 -0400

    update

--- src/Components/App Bar/TopAppBar.js
+++ src/Components/App Bar/TopAppBar.js
@@ -1,19 +1,44 @@
 import { AppBar, Typography, Avatar, Stack } from '@mui/material'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { StyledButton } from '../Styled MUI components/StyledButton'
 import { StyledToolbar } from '../Styled MUI components/StyledToolbar'
 import { Link } from 'react-router-dom'
+import { logout } from '../../actions/auth'
+import { useNavigate, useLocation } from 'react-router-dom'
+import decode from 'jwt-decode'
 
 function TopAppBar() {
+    const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')))
+    const navigate = useNavigate()
+    const location = useLocation()
+
+    useEffect(() => {
+        const token = user?.token
+        if (token) {
+            const decodedToken = decode(token)
+            if (decodedToken.exp * 1000 < new Date().getTime()) logout(setUser, navigate)
+        }
+        setUser(JSON.parse(localStorage.getItem('profile')))
+    }, [location])
+
     return (
         <AppBar position='sticky'>
             <StyledToolbar>
                 <Stack spacing={1} sx={{ display: { xs: 'none', sm: 'block' } }}>
-                    <Avatar>E</Avatar>
-                    <Typography variant='h6'>Eilaf Aljundi</Typography>
+                    {
+                        user && <>
+                            <Avatar>{user.result.name.charAt(0)}</Avatar>
+                            <Typography variant='h6'>{user.result.name}</Typography>
+                        </>
+                    }
+
                 </Stack>
                 <Typography variant='h3' component={Link} to='/' color='primary' sx={{ textDecoration: 'none' }}>PROJECT DEADLINES</Typography>
-                <StyledButton component={Link} to='/auth' variant='contained' color='primary' sx={{ height: 50, float: 'right' }}>Sign in</StyledButton>
+                {
+                    user ?
+                        <StyledButton onClick={logout(setUser, navigate)} variant='contained' color='primary' sx={{ height: 50, float: 'right' }}>Logout</StyledButton>
+                        : <StyledButton component={Link} to='/auth' variant='contained' color='primary' sx={{ height: 50, float: 'right' }}>Sign in</StyledButton>
+                }
             </StyledToolbar>
         </AppBar>
     )
