4cc14dfbb68d2992b822958e8ab425f51167400a
Eilaf Aljundi <elafjundi@gmail.com>
2022-09-15 19:52:11 -0400

    show only the projects for user that is signed in

--- src/Components/Projects Feed/Ongoing Projects/Projects.js
+++ src/Components/Projects Feed/Ongoing Projects/Projects.js
@@ -1,14 +1,16 @@
 import { CircularProgress, Stack } from '@mui/material'
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
+import { useAuth } from '../../../contexts/AuthContext'
 import OngoingCard from './OngoingCard'
 
 function Projects() {
+    const { user } = useAuth()
     const [projects, setProjects] = useState({})
 
     useEffect(() => {
         async function getProjectInfo() {
-            const projectInfo = await axios.get('http://localhost:5000/projects/ongoing')
+            const projectInfo = await axios.get('http://localhost:5000/projects/ongoing?userEmail=' + user.result.email)
             setProjects(projectInfo.data)
         }
         getProjectInfo()
@@ -17,7 +19,7 @@ function Projects() {
     return (
 
         !projects.length ?
-            <CircularProgress size={50} sx={{ p: 40, marginLeft: 30 }} />
+            <CircularProgress size={50}/>
             : (
                 <>
                     {
