ccc9ca75b9b91d2a2894dec5a1b687351a4e05ec
Eilaf Aljundi <elafjundi@gmail.com>
2022-09-22 17:44:11 -0400

    removed server folder from the project

--- server/models/task.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose"
-
-const taskSchema = mongoose.Schema({
-    description: { type: String, required: true },
-    projectId: { type: String, required: true },
-    id: { type: String }
-})
-
-export default mongoose.model('Task', taskSchema)
