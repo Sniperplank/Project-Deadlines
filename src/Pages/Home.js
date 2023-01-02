9fb943a29386177dc6ab458c68b2bdfefe8914af
Eilaf Aljundi <elafjundi@gmail.com>
2023-01-02 03:08:09 -0500

    home page fixes

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -5,8 +5,6 @@ import { useAuth } from '../contexts/AuthContext'
 import { useTheme } from '../contexts/ThemeContext';
 import { Link } from 'react-router-dom'
 
-
-
 function Home() {
     const { user } = useAuth()
     const { isDarkMode } = useTheme()
@@ -28,8 +26,8 @@ function Home() {
         <Stack spacing={10} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }} p={{ xs: 1, sm: 10 }} pl={{ xs: 1, sm: 20 }} pr={{ xs: 1, sm: 20 }}>
             <Typography variant='h4' textAlign='center' color='primary'>Good {timeOfDay}, {user ? user.result.name : 'sign in to get started'}</Typography>
             <hr style={{ width: '50%'}}></hr>
-            <Stack spacing={10} sx={{ width: { xs: '90%', sm: '70%' }, height: 400, background: isDarkMode ? 'linear-gradient(209deg, rgba(38,35,29,1) 18%, rgba(78,54,22,1) 57%, rgba(113,71,16,1) 88%, rgba(147,87,10,1) 100%)' : 'linear-gradient(209deg, rgba(250,246,237,1) 18%, rgba(250,221,182,1) 57%, rgba(250,198,131,1) 88%, rgba(250,174,76,1) 100%)', borderRadius: 20 ,display: 'flex', justifyContent: 'center', alignItems: 'center', boxShadow: '0px 0px 40px #fb8c00' }}>
-                <Typography variant='h5'>Planning and managing projects made easier</Typography>
+            <Stack spacing={5} sx={{ width: { xs: '90%', sm: '70%' }, height: 400, background: isDarkMode ? 'linear-gradient(209deg, rgba(38,35,29,1) 18%, rgba(78,54,22,1) 57%, rgba(113,71,16,1) 88%, rgba(147,87,10,1) 100%)' : 'linear-gradient(209deg, rgba(250,246,237,1) 18%, rgba(250,221,182,1) 57%, rgba(250,198,131,1) 88%, rgba(250,174,76,1) 100%)', borderRadius: 20 ,display: 'flex', justifyContent: 'center', alignItems: 'center', boxShadow: '0px 0px 40px #fb8c00' }}>
+                <Typography variant='h6' textAlign='center'>Planning and managing projects made easier. With this app, you can set deadlines to your projects and plan them accordingly so you can finish them on time. </Typography>
                 <StyledButton component={Link} to={user ? '/ongoing' : '/auth'} variant='contained' color='primary' sx={{height: 40}}>get started</StyledButton>
             </Stack>
         </Stack>
